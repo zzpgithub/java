7ca1ae3e79eae6d9d926b506a97595fe9e9b84df
zzpgithub <2269225970@qq.com>
2018-09-09 16:05:32 +0800

    [Zhipeng Zeng] Complete Matrix class and pass MatrixTest

--- src/test/java/com/cultivation/javaBasicExtended/matrixMultiplication/Matrix.java
+++ src/test/java/com/cultivation/javaBasicExtended/matrixMultiplication/Matrix.java
@@ -1,9 +1,6 @@
 package com.cultivation.javaBasicExtended.matrixMultiplication;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.Arrays;
-import java.util.Objects;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
 class Matrix {
@@ -16,19 +13,57 @@ class Matrix {
     public Matrix(int[][] matrixArray) {
         // TODO: please implement the constructor of a matrix.
         // <--start
-        throw new NotImplementedException();
+        if(matrixArray == null){
+            throw new IllegalArgumentException("Raw matrix is null");
+        }
+        else if(matrixArray.length == 0){
+            throw new IllegalArgumentException("Raw matrix contains 0 row");
+        }
+
+        for(int i = 0; i< matrixArray.length; i++) {
+            if (matrixArray[i] == null) {
+                throw new IllegalArgumentException("Raw matrix contains null row");
+            }
+            else if(matrixArray[i].length == 0){
+                throw new IllegalArgumentException("At least one row of raw matrix contains 0 column");
+            }
+            else if(matrixArray[i].length != matrixArray[0].length){
+                throw new IllegalArgumentException("Raw matrix is not rectangle");
+            }
+        }
+        storage = matrixArray;
+        //throw new NotImplementedException();
         // --end-->
     }
 
     public static Matrix multiply(Matrix left, Matrix right) {
         // TODO: please implement the method to pass the tests.
         // <--start
-        throw new NotImplementedException();
+        if (left == null || right == null)
+            throw new IllegalArgumentException();
+
+        else if (left.columns() != right.rows()) {
+            throw new IllegalArgumentException();
+
+        }
+        Matrix result = new Matrix(new int [left.rows()][right.columns()]);
+        for (int row = 0; row < left.rows(); row++){
+            for (int column = 0; column < right.columns(); column++) {
+                for (int index = 0; index < left.columns(); index++) {
+                    result.getStorage()[row][column] = result.getStorage()[row][column] + left.getStorage()[row][index] * right.getStorage()[index][column];
+                }
+            }
+        }
+        return result;
         // --end-->
     }
 
     // TODO: you can add some helper method if you like.
     // <--start
+
+    public int[][] getStorage() {
+        return storage;
+    }
 
     // --end->
 
