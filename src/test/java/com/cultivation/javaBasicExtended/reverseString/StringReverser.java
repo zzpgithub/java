c8061c4570ab742648752bfd12d134fcc0ad5da1
zzpgithub <2269225970@qq.com>
2018-09-09 16:35:52 +0800

    [Zhipeng Zeng] Complete StringReverser and pass ReverseStringTest

--- src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
@@ -7,7 +7,22 @@ class StringReverser {
     public static String[] reverse(String input) {
         // TODO: please implement the method to pass all the tests.
         // <--start
-        throw new NotImplementedException();
+        if(input == null){
+            throw new IllegalArgumentException();
+        }
+
+        if(input.isEmpty() || input.trim().isEmpty()){
+            return new String[0];
+        }
+
+        String[] result = input.split(" ");
+        for (int i = 0; i < result.length/2; i++) {
+            String temp = result[i];
+            result[i] = result[result.length-1-i];
+            result[result.length-1-i] = temp;
+        }
+        return result;
+        //throw new NotImplementedException();
         // --end-->
     }
 }
